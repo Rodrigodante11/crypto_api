cdc7518f54b9d387abb8047ae5638a27c798c0f1
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2023-01-07 17:56:15 -0300

    usando a anotacao bean para insercao automatica de moedas

--- src/main/java/com/gm2/cryptoapp/controller/CoinController.java
+++ src/main/java/com/gm2/cryptoapp/controller/CoinController.java
@@ -2,10 +2,12 @@ package com.gm2.cryptoapp.controller;
 
 import com.gm2.cryptoapp.entity.Coin;
 import com.gm2.cryptoapp.repository.CoinRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.sql.Timestamp;
 
 @RestController
@@ -16,6 +18,35 @@ public class CoinController {
 
     public CoinController(CoinRepository coinRepository) {
         this.coinRepository = coinRepository;
+    }
+
+    @Bean
+    public Coin init(){
+
+        // inicia auromaticamente crinado os dados abaixo para insercao
+        Coin c1 = new Coin();
+        c1.setName("BITCOIN");
+        c1.setPrice(new BigDecimal( 100));
+        c1.setQuantity(new BigDecimal( 0.0005));
+        c1.setDatetime(new Timestamp(System.currentTimeMillis()));
+
+        Coin c2 = new Coin();
+        c2.setName("BITCOIN");
+        c2.setPrice(new BigDecimal( 150));
+        c2.setQuantity(new BigDecimal( 0.0025));
+        c2.setDatetime(new Timestamp(System.currentTimeMillis()));
+
+        Coin c3 = new Coin();
+        c3.setName("ETHERIUM");
+        c3.setPrice(new BigDecimal( 500));
+        c3.setQuantity(new BigDecimal( 0.0045));
+        c3.setDatetime(new Timestamp(System.currentTimeMillis()));
+
+        coinRepository.insert(c1);
+        coinRepository.insert(c2);
+        coinRepository.insert(c3);
+
+        return c1;
     }
 
     @GetMapping("/{name}")
