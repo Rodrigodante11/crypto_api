a835f4d9dbf8e86f211d793aae164e3f2e82a8b6
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2023-01-07 16:57:22 -0300

    pegando as coin (get/select)

--- src/main/java/com/gm2/cryptoapp/controller/CoinController.java
+++ src/main/java/com/gm2/cryptoapp/controller/CoinController.java
@@ -4,21 +4,23 @@ import com.gm2.cryptoapp.entity.Coin;
 import com.gm2.cryptoapp.repository.CoinRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.Timestamp;
 
 @RestController
 @RequestMapping("/coin")
 public class CoinController {
-    
+
     private CoinRepository coinRepository;
 
     public CoinController(CoinRepository coinRepository) {
         this.coinRepository = coinRepository;
+    }
+
+    @GetMapping()
+    public ResponseEntity get(){
+        return new ResponseEntity<>(coinRepository.getAll(), HttpStatus.OK);
     }
 
     @PostMapping()
--- src/main/java/com/gm2/cryptoapp/repository/CoinRepository.java
+++ src/main/java/com/gm2/cryptoapp/repository/CoinRepository.java
@@ -3,12 +3,19 @@ package com.gm2.cryptoapp.repository;
 import com.gm2.cryptoapp.entity.Coin;
 import jdk.jfr.Enabled;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 @Repository
 public class CoinRepository {
 
     private static String INSERT = "insert into coin(name, price, quantity, datetime) values (?,?,?,?)";
+
+    private static String SELECT_ALL =  "select name, sum(quantity) as quantity from coin group by name";
 
     private JdbcTemplate jdbcTemplate;
 
@@ -28,4 +35,19 @@ public class CoinRepository {
         jdbcTemplate.update(INSERT, attr);
         return coin;
     }
+
+    public List<Coin> getAll(){
+        return jdbcTemplate.query(SELECT_ALL, new RowMapper<Coin>() {
+            @Override
+            public Coin mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Coin coin = new Coin();
+                coin.setName(rs.getString("name"));
+                coin.setQuantity(rs.getBigDecimal("quantity"));
+
+                return coin;
+            }
+        });
+    }
+
+
 }
